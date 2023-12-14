fbd2666c780c1eb8ec6a83da2c77404ebf586700
lucasdprx <l.darpeix@lahorde.school>
2023-12-14 10:49:47 +0100

    Fix Cleric Heal

--- Assets/Script/PlayerAttack/SpecialAttack/ClericSpecialAttack.cs
+++ Assets/Script/PlayerAttack/SpecialAttack/ClericSpecialAttack.cs
@@ -20,8 +20,8 @@ public class ClericSpecialAttack : SpecialAttack
         {
             c.HealNb(c.MaxHp * HealAmount);
             healthBar.UpdateHealthBar();
-            NumberHealPerStage--;
         }
+        NumberHealPerStage--;
         return true;
     }
 }
