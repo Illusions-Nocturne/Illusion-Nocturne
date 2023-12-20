801593adeb7187428f85a9638187adc899c8ed1d
Mathieu Laval <m.laval@lahorde.school>
2023-12-20 13:48:23 +0000

    stop movement in teleporte cheat code

--- Assets/Script/Player/Teleporter.cs
+++ Assets/Script/Player/Teleporter.cs
@@ -6,8 +6,16 @@ public class Teleporter : MonoBehaviour
 {
     [SerializeField] private Transform[] tpPoints;
 
+    private MovePlayer move;
+
+    private void Start()
+    {
+        move = GetComponent<MovePlayer>();
+    }
+
     public void TeleportToTransform(int index)
     {
+        move.StopMovement();
         transform.position = tpPoints[index].position;
     }
 }
